b9f4eee8d7fb3a2a1206af7bad121dbb0160f086
hirusenevirathne <107979195+hirusenevirathne@users.noreply.github.com>
2023-08-18 15:45:42 +0530

    add map and map pin method
    add leaflet map and popup msg to JS file

--- script.js
+++ script.js
@@ -12,15 +12,49 @@ const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
 if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition( //  call the API and get the current position
-        function (position) { // success callback
-            const { latitude } = position.coords;
-            const { longitude } = position.coords;
-            console.log(latitude, longitude);
-            console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
-},
-        function () { // error callback
-            alert('Could not get your position');
-        }
-    );
-};
+  navigator.geolocation.getCurrentPosition(
+    //  call the API and get the current position
+    function (position) {
+      // success callback
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(latitude, longitude);
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+      const coords = [latitude, longitude]; // store the coordinates in an array
+
+      const map = L.map('map').setView(coords, 16); // store map in a variable
+
+      L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      // add a marker
+      map.on('click', function (mapEvent) {
+        // listen to the click event on the map
+        console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng; // get the coordinates of the click event
+
+        L.marker([lat, lng]) // create a marker
+          .addTo(map) // add a marker to the map
+          .bindPopup(
+            L.popup({
+              //setup popup
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false, // popup will not auto close
+              closeOnClick: false, // popup will not close when we click on the map
+              className: 'running-popup', // add class to popup
+            })
+          ) //bind popup to the marker
+          .setPopupContent('Workout') // add content to the popup
+          .openPopup(); // open the popup
+      });
+    },
+    function () {
+      // error callback
+      alert('Could not get your position');
+    }
+  );
+}
