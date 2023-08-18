586ee0e5270ee8d7113bbea08dd2f72885487983
hirusenevirathne <107979195+hirusenevirathne@users.noreply.github.com>
2023-08-18 12:22:47 +0530

    copyright

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition( //  call the API and get the current position
+        function (position) { // success callback
+            const { latitude } = position.coords;
+            const { longitude } = position.coords;
+            console.log(latitude, longitude);
+            console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+},
+        function () { // error callback
+            alert('Could not get your position');
+        }
+    );
+};
